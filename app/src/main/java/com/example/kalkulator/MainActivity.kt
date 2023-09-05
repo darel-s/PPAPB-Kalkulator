c7914b2f775a2e1bd1b9cce5903ac897f61fadbe
Darriel2603 <darrielmarkerizal@gmail.com>
2023-09-05 19:08:03 +0700

    first commit

---
+++ app/src/main/java/com/example/kalkulator/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kalkulator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+}
+
+
